9cc66857c7228b5064cf6d45a2b436814ae6ca48
FatttSnake <fatttsnake@gmail.com>
2023-11-20 22:46:19 +0800

    Rename GitHubAvatar

--- avatar-generator/src/main/kotlin/top/fatweb/avatargenerator/GitHubAvatar.kt
+++ avatar-generator/src/main/kotlin/top/fatweb/avatargenerator/GitHubAvatar.kt
@@ -2,7 +2,7 @@ package top.fatweb.avatargenerator
 
 import top.fatweb.avatargenerator.element.GithubElementRegistry
 
-object GithubAvatar {
+object GitHubAvatar {
     fun newAvatarBuilder() = Avatar.newBuilder().elementRegistry(GithubElementRegistry())
 
     fun newAvatarBuilder(size: Int, precision: Int) =
